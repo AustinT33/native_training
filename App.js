f0959569bb45c52121247655557b52b6cb510283
austint33 <austintumlinson@gmail.com>
2020-08-01 17:55:09 -0500

    adding functionality with background color changing buttons

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+
+function ColorButton({ backgroundColor, onPress = f => f }) {
+  return (
+      <TouchableHighlight style={styles.button}
+        onPress={() => onPress(backgroundColor)}
+        underlayColor='grey'
+        >
+        <View style={styles.row}>
+          <View style={[styles.sample, { backgroundColor }]}/>
+          <Text style={styles.buttonText}>{backgroundColor}</Text>
+        </View>
+      </TouchableHighlight>
+  )
+}
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState('blue');
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <TouchableHighlight style={styles.button}
-        onPress={() => setBackgroundColor('yellow')}
-        underlayColor='orange'
-        >
-        <View style={styles.row}>
-          <View style={[styles.sample, { backgroundColor: 'yellow' }]}/>
-          <Text style={styles.buttonText}>Yellow</Text>
-        </View>
-      </TouchableHighlight>
+      <ColorButton backgroundColor='red' onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor='green' onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor='blue' onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor='yellow' onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor='purple' onPress={setBackgroundColor}/>
+      <StatusBar style="auto" />
     </View>
   );
 }
