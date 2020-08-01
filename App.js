a2010f69407ce0a27826fa03b2d0488d0a712b11
austint33 <austintumlinson@gmail.com>
2020-08-01 17:48:21 -0500

    working with styling/events for views

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState('blue');
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <Text style={styles.button}
-        onPress={ () => setBackgroundColor('green')}>Green</Text>
-      <Text style={styles.button}
-        onPress={ () => setBackgroundColor('red')}>Red</Text>
+      <TouchableHighlight style={styles.button}
+        onPress={() => setBackgroundColor('yellow')}
+        underlayColor='orange'
+        >
+        <View style={styles.row}>
+          <View style={[styles.sample, { backgroundColor: 'yellow' }]}/>
+          <Text style={styles.buttonText}>Yellow</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -17,11 +22,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   button: {
-    fontSize: 30,
     margin: 10,
     padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    backgroundColor: 'rgba(255,255,255,.8)'
   },
+  buttonText: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  sample: {
+    height: 20,
+    width: 20,
+    margin: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  }
 })
