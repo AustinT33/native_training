255960bdfda35fcd78ee6cc69c0baefd9af5f8c1
austint33 <austintumlinson@gmail.com>
2020-08-02 18:28:10 -0500

    functional color app with adding colors and viewing their details

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import Color from 'color';
 
-export default function ColorDetails() {
+export default function ColorDetails({ route }) {
+    const { color: name } = route.params
+    const color = Color(name);
+    const textColor = { fontSize: 30, color: color.negate().toString() };
   return (
-      <View style={styles.container}>
-        <Text>Color Details</Text>
+      <View style={[styles.container, {backgroundColor: name }]}>
+        <Text style={textColor}>{name}</Text>
+        <Text style={textColor}>{color.rgb().toString()}</Text>
+        <Text style={textColor}>{color.hsl().toString()}</Text>
+        <Text style={textColor}>{color.luminosity()}</Text>
       </View>
   );
 }
