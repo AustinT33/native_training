f5882178670e0f682d6935fa80f7cb1a0107f3ab
austint33 <austintumlinson@gmail.com>
2020-08-02 16:44:28 -0500

    adding hooks for text value

--- components/ColorForm.js
+++ components/ColorForm.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function ColorForm() {
+    const [inputValue, setValue] = useState('');
     return (
         <View style={styles.container}>
-            <TextInput style={styles.txtInput} />
+            <TextInput 
+                style={styles.txtInput} 
+                autoCapitalize='none' 
+                value={inputValue}
+                onChangeText={setValue}
+            />
             <Button title='add' />
         </View>
     );
@@ -13,10 +19,15 @@ export default function ColorForm() {
 const styles = StyleSheet.create({
     container: {
         marginTop: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     txtInput: {
         flex: 1,
         borderWidth: 2,
         fontSize: 20,
+        margin: 5,
+        borderRadius: 5,
+        padding: 5,
     }
 })
