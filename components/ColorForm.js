9dfd965e5709405444f4fe1e7f7791fdbdec47dd
austint33 <austintumlinson@gmail.com>
2020-08-02 16:40:49 -0500

    adding styles for textInput

--- components/ColorForm.js
+++ components/ColorForm.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function ColorForm() {
     return (
-        <View>
-            <Text>Ready</Text>
+        <View style={styles.container}>
+            <TextInput style={styles.txtInput} />
+            <Button title='add' />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        marginTop: 40,
+    },
+    txtInput: {
+        flex: 1,
+        borderWidth: 2,
+        fontSize: 20,
     }
 })
