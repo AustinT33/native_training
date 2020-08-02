4ee62ea33af5d4b24f7a07427abb16f310d51e13
austint33 <austintumlinson@gmail.com>
2020-08-02 17:25:16 -0500

    implementing useEffect and AsyncStorage for setting and retrieving

--- hooks.js
+++ hooks.js
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { generate } from 'shortid';
+import { AsyncStorage } from 'react-native';
 
 export const useColors = () => {
   const [colors, setColors] = useState([]);
+
+  const loadColors = async () => {
+      const colorData = await AsyncStorage.getItem('@ColorListStore:Colors');
+      if (colorData) {
+          const colors = JSON.parse(colorData);
+          setColors(colors);
+      }
+  }
+
+  useEffect(() => {
+      if (colors.length) return;
+      loadColors();
+  }, []);
+
+  useEffect(() => {
+      AsyncStorage.setItem('@ColorListStore:Colors', JSON.stringify(colors));
+  }, [colors]);
+
+
   const addColor = color => {
     const newColor = { id: generate(), color };
     setColors([ newColor, ...colors ]);
