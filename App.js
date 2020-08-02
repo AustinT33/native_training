ab3210c4816c4e4aa0955e9a00175d8491f97465
austint33 <austintumlinson@gmail.com>
2020-08-02 16:37:33 -0500

    finished colorbutton adding colorform

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import ColorButton from './components/ColorButton';
+import defaultColors from './data/defaultColors.json';
+import ColorForm from './components/ColorForm';
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState('blue');
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <ColorButton backgroundColor='red' onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor='green' onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor='blue' onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor='yellow' onPress={setBackgroundColor}/>
-      <ColorButton backgroundColor='purple' onPress={setBackgroundColor}/>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <ColorForm />
+      <FlatList 
+        style={[styles.container, { backgroundColor }]} 
+        data={defaultColors}
+        renderItem={({ item }) => {
+          return (
+            <ColorButton 
+              key={item.id} 
+              backgroundColor={item.color} 
+              onPress={setBackgroundColor} 
+            />
+          );
+        }}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: 'flex',
   },
 })
---
+++ components/ColorForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ColorForm() {
+    return (
+        <View>
+            <Text>Ready</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
